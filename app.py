5c923f304d72b0410b26c2ae4ebd27e6e36c3a37
Harsh Pandey <119357977+harsh2508pandey@users.noreply.github.com>
2025-11-06 12:33:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,4 @@ def weather():
         return jsonify({"error": f"Could not fetch weather for {city}"}), response.status_code
 
 if __name__ == '__main__':
-    # Run on all interfaces
     app.run(host='0.0.0.0', port=5000)
